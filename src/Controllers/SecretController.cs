9a67da7cdcdb39f7631e33e1b44ca4026537f26a
Luis Alexandre Rodrigues <luis.rodrigues@aarin.com.br>
2021-03-30 11:29:26 -0300

    Initial Setup

---
+++ src/Controllers/SecretController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace AppKeyVaultSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SecretController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SecretController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var connectionString = _configuration.GetValue<string>("ConnectionString");
+            var authApiKey = _configuration.GetValue<string>("ApiKey");
+
+            return Ok(new { ConnectionString = connectionString, AuthApiKey = authApiKey });
+        }
+    }
+}
